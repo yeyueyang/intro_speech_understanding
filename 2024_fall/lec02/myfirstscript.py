5f8348c9b06bf793e3ea7daa2f61e3a5c6c13941
WANG RUXUAN <yueyangye02@gmail.com>
2025-01-30 03:37:34 +0800

    homework 2
    homework 2

---
+++ 2024_fall/lec02/myfirstscript.py
@@ -0,0 +1,8 @@
+"""
+This file, myfirstscript.py, is an exanple.
+
+This is a multi-line string.
+A string at the beginning of a file is called a docstring.
+"""
+
+print("hello world, this is python!")
